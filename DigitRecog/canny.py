384bbb5116be74b61e9687fe3609b12cad0f7fc8
Shreyas-hacker <shreyashegde1@gmail.com>
2022-04-16 03:23:56 +0800

    changing images to canny edges

--- DigitRecog/canny.py
+++ DigitRecog/canny.py
@@ -1,23 +1,26 @@
 import cv2
 import os
+import PIL
+import numpy as np
+from PIL import Image
 from cv2 import threshold
 
 directory = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/trainingSet/0'
+new_direc = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/trainingSet/new_0'
 count = 1
 for image in os.listdir(directory):
     image_path = os.path.join(directory, image)
     img = cv2.imread(image_path)
-    cv2.imshow('Original',img)
-    cv2.waitKey(0)
 
     img_size = cv2.resize(img,(28,28))
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img_blur = cv2.GaussianBlur(img_gray,(3,3), 0)
 
     edges = cv2.Canny(image=img_blur, threshold1=100,threshold2=200)
-    file = directory + f'0_img({count})'
+    file = new_direc + f'0_img({count}).jpeg'
     count+=1
-    cv2.imread(file,edges)
-    cv2.waitKey(0)
+    image_tosave = Image.fromarray(edges.astype(np.uint8))
+    print(image_tosave)
+    image_tosave.save(file, 'JPEG')
 
 cv2.destroyAllWindows()
