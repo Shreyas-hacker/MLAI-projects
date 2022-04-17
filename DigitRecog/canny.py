1658aa564ba31e9f2776e8238dea0fd8c811c6ba
Shreyas-hacker <shreyashegde1@gmail.com>
2022-04-17 23:32:24 +0800

    canny edged pictures

--- DigitRecog/canny.py
+++ DigitRecog/canny.py
@@ -5,22 +5,22 @@ import numpy as np
 from PIL import Image
 from cv2 import threshold
 
-directory = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/trainingSet/9'
-new_direc = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/trainingSet/new_9'
-count = 1
-for image in os.listdir(directory):
-    image_path = os.path.join(directory, image)
-    img = cv2.imread(image_path)
+for i in range(0,10):
+    directory = f'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/{i}'
+    new_direc = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/trainingSet/trainingSet/'
+    count = 1
+    for image in os.listdir(directory):
+        image_path = os.path.join(directory, image)
+        img = cv2.imread(image_path)
 
-    img_size = cv2.resize(img,(28,28))
-    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(img_gray,(3,3), 0)
+        img_size = cv2.resize(img,(28,28))
+        img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(img_gray,(3,3), 0)
 
-    edges = cv2.Canny(image=img_blur, threshold1=100,threshold2=200)
-    file = new_direc + f'0_img({count}).jpeg'
-    count+=1
-    image_tosave = Image.fromarray(edges.astype(np.uint8))
-    print(image_tosave)
-    image_tosave.save(file, 'JPEG')
+        edges = cv2.Canny(image=img_blur, threshold1=100,threshold2=200)
+        file = new_direc + f'{i}_img({count}).jpeg'
+        count+=1
+        image_tosave = Image.fromarray(edges.astype(np.uint8))
+        image_tosave.save(file, 'JPEG')
 
 cv2.destroyAllWindows()
