c363be46edef76f9d0b1c0d0561168c12eba742d
Shreyas-hacker <shreyashegde1@gmail.com>
2022-04-18 21:25:31 +0800

    new changes

--- DigitRecog/canny.py
+++ DigitRecog/canny.py
@@ -6,14 +6,17 @@ from PIL import Image
 from cv2 import threshold
 
 for i in range(0,10):
-    directory = f'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/testSet/{i}'
-    new_direc = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/MLAI projects/DigitRecog/Dataset/testSet/testSet/'
+    directory = f'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/Dataset/trainingSet/trainingSet/{i}'
+    new_direc = 'C:/Users/shrey/OneDrive - Nanyang Technological University/Desktop/Dataset/trainingSet/trainingSet/FinalSet/'
     count = 1
     for image in os.listdir(directory):
-        image_path = os.path.join(directory, image)
-        img = cv2.imread(image_path)
+        try:
+            path=os.path.join(directory, image)
+            img=cv2.imread(path)
+            img=cv2.resize(img, (28, 28))
 
-        img_size = cv2.resize(img,(28,28))
+        except Exception as e:
+            print(str(e))
         img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         img_blur = cv2.GaussianBlur(img_gray,(3,3), 0)
 
